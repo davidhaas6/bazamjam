4f4871acd0e61c80e6cac9454dec21c9e136e670
David Haas <davidhaas6@gmail.com>
2021-09-12 16:54:49 -0500

    Visualizer set up

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,115 +2,63 @@ import React, { ReactElement } from 'react';
 import './App.css';
 import RecordingButton from './RecordingButton';
 import ToggleButton from 'react-bootstrap/ToggleButton'
+import AudioManager from './AudioManager';
 
-type AppProps = {
-};
 
 type AppState = {
-  recording: boolean; // like this
-  recorder?: MediaRecorder;
+  audio: MediaStream | null; // like this
 };
 
 
 
-class App extends React.Component<AppProps, AppState> {
+class App extends React.Component<{}, AppState> {
   BUFFER_MS: number = 50;
+  // state: AppState =
+  constructor() {
+    super({});
 
-  state: AppState = {
-    recording: false
+    this.state = {
+      audio: null
+    };
+
+    this.toggleMicrophone = this.toggleMicrophone.bind(this);
   }
 
-  startBtnClbk = (): void => {
-    let newRecordingState = !this.state.recording; // todo: possible race conditions?
-    this.setState((state) => ({ recording: newRecordingState }));
 
-    if (newRecordingState === true) {
-      this.startStream();
+  async getMicrophone() {
+    const audio = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: false
+    });
+    this.setState({ audio });
+  }
+
+  stopMicrophone() {
+    if (this.state.audio) {
+      this.state.audio.getTracks().forEach(track => track.stop());
+      this.setState({ audio: null });
+    }
+  }
+
+  toggleMicrophone() {
+    if (this.state.audio) {
+      this.stopMicrophone();
     } else {
-      this.stopStream();
+      this.getMicrophone();
     }
   }
 
-  startStream(): void {
-    if (this.state.recorder === undefined) {
-      this.initStream();
-    } else {
-      if (this.state.recorder.state !== "recording") {
-        this.state.recorder.start(this.BUFFER_MS);
-      }
-    }
-  }
-
-  initStream(): void {
-    // Initializes and starts an audio stream
-    navigator.mediaDevices.getUserMedia({ audio: true })
-      .then(stream => {
-        console.log(stream);
-        let mediaRecorder = new MediaRecorder(stream, {mimeType: "audio/wav"});
-        mediaRecorder.ondataavailable = this.audioCallback;
-        mediaRecorder.start(this.BUFFER_MS);
-
-    //         const context = new AudioContext();
-    // const source = context.createMediaStreamSource(mediaRecorder.stream);
-    // const processor = context.createScriptProcessor(1024, 1, 1);
-
-    // source.connect(processor);
-    // processor.connect(context.destination);
-
-    // processor.onaudioprocess = function(e) {
-    //   // Do something with the data, e.g. convert it to WAV
-    //   console.log(e.inputBuffer);
-    // };
-
-        this.setState({ recorder: mediaRecorder });
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }
-
-  stopStream(): void {
-    if (this.state.recorder) {
-      this.state.recorder.stop();
-    }
-  }
-
-  audioCallback = (event: BlobEvent) => {
-    let blob: Blob = event.data;
-    console.log(blob.size, blob.type);
-    blob.arrayBuffer().then(this.processBuffer);
-    // const context = new AudioContext();
-    // const source = context.createMediaStreamSource(recorder.stream);
-    // const processor = context.createScriptProcessor(1024, 1, 1);
-
-    // source.connect(processor);
-    // processor.connect(context.destination);
-
-    // processor.onaudioprocess = function(e) {
-    //   // Do something with the data, e.g. convert it to WAV
-    //   console.log(e.inputBuffer);
-    // };
-  }
-
-  processBuffer(buffer: ArrayBuffer) {
-    
-  }
 
   render(): ReactElement {
-    console.log("rendering with recording state = " + this.state.recording);
-
+    const audio = this.state.audio;
+    console.log("rendering // is audio null? " + (audio === null));
 
     return (
       <div className="App">
-        <header className="App-header">
-
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-
-
-          <RecordingButton isRecording={this.state.recording} onClick={this.startBtnClbk} />
-        </header>
+        <div className="controls">
+          {audio != null ? <AudioManager audio={audio}/> : ""}
+          <RecordingButton isRecording={this.state.audio !== null} onClick={this.toggleMicrophone} />
+        </div>
       </div>
     );
   }
