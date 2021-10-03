c030bd5a8aa2ed462cf1ef2b049a5e9424913578
David Haas <davidhaas6@gmail.com>
2021-10-02 23:09:51 -0500

    functional grid

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -3,20 +3,21 @@
 import { Component, FunctionComponent, ReactElement, useState } from "react";
 
 // grid
-import { Responsive as ResponsiveGridLayout, Layout } from 'react-grid-layout';
-import ReactGridLayout from "react-grid-layout";
+import RGL, { WidthProvider, Layout, ReactGridLayoutProps } from 'react-grid-layout';
+// import ReactGridLayout from "react-grid-layout";
 import '../assets/grid_styles.css';
 import '../assets/resizable_styles.css';
 
+const ReactGridLayout = WidthProvider(RGL);
 
 interface IDashboardProps {
 
 }
 
 const defaultComponents: ReactElement[] = [
-  <div key="a" className="dashboard-component">1</div>,
-  <div key="b" className="dashboard-component" >2</div>,
-  <div key="c" className="dashboard-component">3</div>,
+  <div key="a" className="dashboard-component">a</div>,
+  <div key="b" className="dashboard-component">b</div>,
+  <div key="c" className="dashboard-component">c</div>,
 ];
 
 interface IBreakpoints {
@@ -27,17 +28,26 @@ interface IBreakpoints {
   xxs?: number
 };
 
-const breakpoints: IBreakpoints = { lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 };
+const breakpoints: IBreakpoints = { lg: 1200, xs: 480};
 type breakpointKeys = keyof IBreakpoints;
 
 
 const defaultLayouts: { [key in breakpointKeys]?: Layout[] } = {
   lg: [
-    { i: 'a', x: 0, y: 0, w: 3, h: 2, static: true },
-    { i: 'b', x: 1, y: 0, w: 3, h: 1, minW: 2, maxW: 4 },
-    { i: 'c', x: 4, y: 0, w: 1, h: 2 }
+    { i: 'a', x: 0, y: 0, w: 1, h: 1, /*static: true*/ },
+    { i: 'b', x: 1, y: 1, w: 1, h: 1, minW: 1, maxW: 2, minH: 1, maxH: 2  },
+    { i: 'c', x: 4, y: 2, w: 1, h: 1 }
   ]
 
+};
+
+const defaultProps: ReactGridLayoutProps = {
+  layout: defaultLayouts.lg,
+  rowHeight: 200,
+  cols: 3,
+  verticalCompact: false,
+  isBounded: true,
+  onLayoutChange: function () { },
 };
 
 const Dashboard: FunctionComponent<IDashboardProps> = (props: IDashboardProps) => {
@@ -45,12 +55,22 @@ const Dashboard: FunctionComponent<IDashboardProps> = (props: IDashboardProps) =
   const [components, setComponents] = useState(defaultComponents);
   const [layouts, setLayouts] = useState(defaultLayouts);
 
-  return (
-    // https://github.com/react-grid-layout/react-grid-layout
-    <ReactGridLayout className="layout" layout={defaultLayouts.lg} cols={3} autoSize={false} >
-      {components}
+  props = defaultProps; //todo
+  props.cols
 
-    </ReactGridLayout>
+  // https://github.com/react-grid-layout/react-grid-layout
+  return (
+    <div className="dashboard">
+
+      <ReactGridLayout className="grid" {...defaultProps}>
+        {components}
+      </ReactGridLayout>
+    </div >
+  );
+}
+
+export default Dashboard;
+
     // <ResponsiveGridLayout className="layout" layouts={layouts}
     //   // breakpoints={breakpoints}
     //   isDroppable={true}
@@ -60,7 +80,3 @@ const Dashboard: FunctionComponent<IDashboardProps> = (props: IDashboardProps) =
     // >
     //   {components}
     // </ResponsiveGridLayout>
-  );
-}
-
-export default Dashboard;
